89731b9fe388e330c60908d027ce17581c088ae7
Alex Naidovich <alexnaidovich@users.noreply.github.com>
2018-02-28 15:21:34 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    var input = equation.split(' '),
+        result = [],
+        a = +input[0],
+        b = +(input[3] + input[4]),
+        c = +(input[7] + input[8]),
+        d = Math.sqrt((b * b) - 4 * a * c);
+    result.push(Math.round((-b - d) / (a * 2)));
+    result.push(Math.round((-b + d) / (a * 2)));
+    
+    if (result[0] > result[1]) {
+        return result.reverse();
+    } else {
+        return result;
+    }
 }
